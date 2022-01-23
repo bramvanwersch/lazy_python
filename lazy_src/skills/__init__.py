7f926c404a527f1a4475c1b914eae8676f50696a
bram van wersch <eckelradekip10@gmail.com>
2022-01-23 21:04:35 +0100

    added all the planned skills

--- lazy_src/skills/__init__.py
+++ lazy_src/skills/__init__.py
@@ -3,9 +3,42 @@ from lazy_src.skills._skills import *
 
 class Skills:
     EXPLORING = Skill("exploring")
+
+    # gathering skills
     GATHERING = Skill("gathering")
     WOODCUTTING = Skill("woodcutting")
     FISHING = Skill("fishing")
+    MINING = Skill("mining")
+    STEALING = Skill("stealing")
+    FARMING = Skill("farming")
+    ARCHEOLOGY = Skill("archeology")
+    HUNTING = Skill("hunting")
+
+    # combat TO BE IMPLEMENTED
+    RANGING = Skill("ranging")
+    FIGHTING = Skill("fighting")
+    SPELLCASTING = Skill("spellcasting")
+
+    # buffing skill TO BE IMPLEMENTED
+    WORSHIPPING = Skill("worshipping")
+
+    # creating skills TO BE IMPLEMENTED
+    # for ranging
+    LEATHERWORKING = Skill("leatherworking")
+    FLECTHING = Skill("fletching")
+
+    # for mage
+    WEAVING = Skill("weaving")
+
+    # for melee
+    ARMOR_SMITHING = Skill("armor_smithing")
+    WEAPON_SMITHING = Skill("weapon_smithing")
+
+    # general
+    BREWING = Skill("brewing")
+    BUILDING = Skill("building")
+    COOKING = Skill("cooking")
+    WRITING = Skill("writing")
 
     @classmethod
     def all_skills(cls):
