8c17d7527fad3dc9680b4fc3d6cd321cd4971287
bram van wersch <eckelradekip10@gmail.com>
2022-01-28 08:15:06 +0100

    dad character and some small changes

--- lazy_src/people/__init__.py
+++ lazy_src/people/__init__.py
@@ -3,3 +3,4 @@ from lazy_src.people.people import *
 
 class People:
     MOM = Person("mom", "The nicest person I know.")
+    DAD = Person("dad", "Ready to help you whenever but lets you know how he feels.")
